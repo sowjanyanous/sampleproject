5efcf2730a75948a2686f39e6d95854e1320f012
sowjanyanous <sowjanyam@nousinfo.com>
2025-10-27 16:54:15 +0530

    root

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 80;
 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello from Node.js ECS CI/CD Test! for test');
-});  */
+});  
 
+/*
 app.get(['/nodejs*', '/nodejs/*'], (req, res) => {
     res.send('Hello from Node.js ECS CI/CD Test! for test');
 });
+*/
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
