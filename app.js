3629c024b6c964b56e1afa6fb89ee3e6b77e355c
sowjanyanous <sowjanyam@nousinfo.com>
2025-11-18 21:40:49 +0530

    Update app.js ..testing1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 80;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js ECS CI/CD Test! for dev env...!!!!');
+    res.send('Hello from Node.js ECS CI/CD Test! for dev env..nextpart.!!!!');
 });  
 
 /*
